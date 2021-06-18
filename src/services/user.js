e9f5ccb776c60aadd8ddbe1eca541f3e5f535d79
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2021-06-18 15:38:14 -0300

    🎉 :tada: initial commit 🎉
    Initial structure of folders, variables, hooks, etc.

---
+++ src/services/user.js
@@ -0,0 +1,27 @@
+import axios from "axios"
+import {BASE_URL} from "../constants/apiConstants"
+import { goToRecipesFeed } from "../routes/coordinator"
+
+export const login = (body, history, setLogged) => {
+    
+    axios.post(`${BASE_URL}/user/login`, body).then(response => {
+        localStorage.setItem("token", response.data.token)
+        setLogged(true)
+        goToRecipesFeed(history)
+    }).catch(error => {
+        alert("Email ou senha inválidos!")
+        console.log(error.message)
+    })
+}
+
+export const signup = (body, history, setLogged) => {
+    
+    axios.post(`${BASE_URL}/user/signup`, body).then(response => {
+        localStorage.setItem("token", response.data.token)
+        setLogged(true)
+        goToRecipesFeed(history)
+    }).catch(error => {
+        console.log(error.message)
+    })
+}
+
